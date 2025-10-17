e73f81ba82ffaf531dbdaa8ccbeb597ea72c3cd5
chu2bard <bxndnbdnd29@gmail.com>
2025-10-17 22:50:13 +0200

    add basic cli

---
+++ tests/test_graph.py
@@ -0,0 +1,36 @@
+import pytest
+from agentplex.graph import Graph
+from agentplex.node import FunctionNode
+from agentplex.errors import CycleError
+
+
+def noop(state):
+    pass
+
+
+def test_add_nodes():
+    g = Graph("test")
+    g.add_node(FunctionNode("a", noop))
+    g.add_node(FunctionNode("b", noop))
+    assert len(g) == 2
+
+
+def test_linear_order():
+    g = Graph("test")
+    g.add_node(FunctionNode("a", noop))
+    g.add_node(FunctionNode("b", noop))
+    g.add_node(FunctionNode("c", noop))
+    g.add_edge("a", "b")
+    g.add_edge("b", "c")
+    order = g.execution_order()
+    flat = [nid for layer in order for nid in layer]
+    assert flat == ["a", "b", "c"]
+
+
+def test_cycle_detection():
+    g = Graph("test")
+    g.add_node(FunctionNode("a", noop))
+    g.add_node(FunctionNode("b", noop))
+    g.add_edge("a", "b")
+    with pytest.raises(CycleError):
+        g.add_edge("b", "a")
